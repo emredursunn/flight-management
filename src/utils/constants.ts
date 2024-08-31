12288693d6cbc8204036e2694714ce7cfae7b45a
emredursunn <emredursun908@gmail.com>
2024-08-31 20:56:48 +0300

    added typography

---
+++ src/utils/constants.ts
@@ -0,0 +1,290 @@
+import { StyleSheet } from "react-native";
+
+export const typography = StyleSheet.create({
+    largeTitleRegular: {
+        fontFamily: 'SF Pro',
+        fontSize: 34,
+        fontWeight: 'regular',
+        lineHeight: 41,
+        letterSpacing: -0.4,
+        color:'#000000'
+      },
+      largeTitleBold: {
+        fontFamily: 'SF Pro',
+        fontSize: 34,
+        fontWeight: 'bold',
+        lineHeight: 41,
+        letterSpacing: -0.4,
+        color:'#000000'
+      },
+    title1Regular: {
+        fontFamily: 'SF Pro',
+        fontSize: 28,
+        fontWeight: 'regular',
+        lineHeight: 34,
+        letterSpacing: -0.4,
+        color:'#000000'
+      },
+      title1Bold: {
+        fontFamily: 'SF Pro',
+        fontSize: 28,
+        fontWeight: 'bold',
+        lineHeight: 34,
+        letterSpacing: -0.4,
+        color:'#000000'
+      },
+    title2Regular: {
+        fontFamily: 'SF Pro',
+        fontSize: 22,
+        fontWeight: 'regular',
+        lineHeight: 28,
+        letterSpacing: -0.4,
+        color:'#000000'
+      },
+      title2Bold: {
+        fontFamily: 'SF Pro',
+        fontSize: 22,
+        fontWeight: 'bold',
+        lineHeight: 28,
+        letterSpacing: -0.4,
+        color:'#000000'
+      },
+    title3Regular: {
+        fontFamily: 'SF Pro',
+        fontSize: 20,
+        fontWeight: 'regular',
+        lineHeight: 25,
+        letterSpacing: -0.4,
+        color:'#000000'
+      },
+      title3Semibold: {
+        fontFamily: 'SF Pro',
+        fontSize: 20,
+        fontWeight: 'semibold',
+        lineHeight: 25,
+        letterSpacing: -0.4,
+        color:'#000000'
+      },
+      headlineSemibold:{
+        fontFamily: 'SF Pro',
+        fontSize: 17,
+        fontWeight: 'semibold',
+        lineHeight: 22,
+        letterSpacing: -0.4,
+        color:'#000000'
+      },
+      headlineSemiboldItalic:{
+        fontFamily: 'SF Pro',
+        fontSize: 17,
+        fontWeight: 'semibold',
+        lineHeight: 22,
+        letterSpacing: -0.4,
+        color:'#000000',
+        fontStyle:'italic'
+    },
+      bodyRegular:{
+        fontFamily: 'SF Pro',
+        fontSize: 17,
+        fontWeight: 'regular',
+        lineHeight: 22,
+        letterSpacing: -0.4,
+        color:'#000000',
+    },
+      bodySemibold:{
+        fontFamily: 'SF Pro',
+        fontSize: 17,
+        fontWeight: 'semibold',
+        lineHeight: 22,
+        letterSpacing: -0.4,
+        color:'#000000',
+    },
+      bodyRegularItalic:{
+        fontFamily: 'SF Pro',
+        fontSize: 17,
+        fontWeight: 'regular',
+        lineHeight: 22,
+        letterSpacing: -0.4,
+        color:'#000000',
+        fontStyle:'italic'
+    },
+      bodySemiboldItalic:{
+        fontFamily: 'SF Pro',
+        fontSize: 17,
+        fontWeight: 'semibold',
+        lineHeight: 22,
+        letterSpacing: -0.4,
+        color:'#000000',
+        fontStyle:'italic'
+    },
+    calloutRegular:{
+        fontFamily: 'SF Pro',
+        fontSize: 16,
+        fontWeight: 'regular',
+        lineHeight: 21,
+        letterSpacing: -0.4,
+        color:'#000000',
+    },
+    calloutSemibold:{
+        fontFamily: 'SF Pro',
+        fontSize: 16,
+        fontWeight: 'semibold',
+        lineHeight: 21,
+        letterSpacing: -0.4,
+        color:'#000000',
+    },
+    calloutRegularItalic:{
+        fontFamily: 'SF Pro',
+        fontSize: 16,
+        fontWeight: 'regular',
+        lineHeight: 21,
+        letterSpacing: -0.4,
+        color:'#000000',
+        fontStyle:'italic'
+    },
+    calloutSemiboldItalic:{
+        fontFamily: 'SF Pro',
+        fontSize: 16,
+        fontWeight: 'semibold',
+        lineHeight: 21,
+        letterSpacing: -0.4,
+        color:'#000000',
+        fontStyle:'italic'
+    },
+    subheadlineRegular:{
+        fontFamily: 'SF Pro',
+        fontSize: 15,
+        fontWeight: 'regular',
+        lineHeight: 20,
+        letterSpacing: -0.4,
+        color:'#000000',
+    },
+    subheadlineSemibold:{
+        fontFamily: 'SF Pro',
+        fontSize: 15,
+        fontWeight: 'semibold',
+        lineHeight: 20,
+        letterSpacing: -0.4,
+        color:'#000000',
+    },
+    subheadlineRegularItalic:{
+        fontFamily: 'SF Pro',
+        fontSize: 15,
+        fontWeight: 'regular',
+        lineHeight: 20,
+        letterSpacing: -0.4,
+        color:'#000000',
+        fontStyle:'italic'
+    },
+    subheadlineSemiboldItalic:{
+        fontFamily: 'SF Pro',
+        fontSize: 15,
+        fontWeight: 'semibold',
+        lineHeight: 20,
+        letterSpacing: -0.4,
+        color:'#000000',
+        fontStyle:'italic'
+    },
+    footnoteRegular:{
+        fontFamily: 'SF Pro',
+        fontSize: 13,
+        fontWeight: 'regular',
+        lineHeight: 18,
+        letterSpacing: -0.4,
+        color:'#000000',
+    },
+    footnoteSemibold:{
+        fontFamily: 'SF Pro',
+        fontSize: 13,
+        fontWeight: 'semibold',
+        lineHeight: 18,
+        letterSpacing: -0.4,
+        color:'#000000',
+    },
+    footnoteRegularItalic:{
+        fontFamily: 'SF Pro',
+        fontSize: 13,
+        fontWeight: 'regular',
+        lineHeight: 18,
+        letterSpacing: -0.4,
+        color:'#000000',
+        fontStyle:'italic'
+    },
+    footnoteSemiboldItalic:{
+        fontFamily: 'SF Pro',
+        fontSize: 13,
+        fontWeight: 'semibold',
+        lineHeight: 18,
+        letterSpacing: -0.4,
+        color:'#000000',
+        fontStyle:'italic'
+    },
+    caption1Regular:{
+        fontFamily: 'SF Pro',
+        fontSize: 12,
+        fontWeight: 'regular',
+        lineHeight: 16,
+        letterSpacing: -0.4,
+        color:'#000000',
+    },
+    caption1Semibold:{
+        fontFamily: 'SF Pro',
+        fontSize: 12,
+        fontWeight: 'semibold',
+        lineHeight: 16,
+        letterSpacing: -0.4,
+        color:'#000000',
+    },
+    caption1RegularItalic:{
+        fontFamily: 'SF Pro',
+        fontSize: 12,
+        fontWeight: 'regular',
+        lineHeight: 16,
+        letterSpacing: -0.4,
+        color:'#000000',
+        fontStyle:'italic'
+    },
+    caption1SemiboldItalic:{
+        fontFamily: 'SF Pro',
+        fontSize: 12,
+        fontWeight: 'semibold',
+        lineHeight: 16,
+        letterSpacing: -0.4,
+        color:'#000000',
+        fontStyle:'italic'
+    },
+    caption2Regular:{
+        fontFamily: 'SF Pro',
+        fontSize: 11,
+        fontWeight: 'regular',
+        lineHeight: 13,
+        letterSpacing: -0.4,
+        color:'#000000',
+    },
+    caption2Semibold:{
+        fontFamily: 'SF Pro',
+        fontSize: 11,
+        fontWeight: 'semibold',
+        lineHeight: 13,
+        letterSpacing: -0.4,
+        color:'#000000',
+    },
+    caption2RegularItalic:{
+        fontFamily: 'SF Pro',
+        fontSize: 11,
+        fontWeight: 'regular',
+        lineHeight: 13,
+        letterSpacing: -0.4,
+        color:'#000000',
+        fontStyle:'italic'
+    },
+    caption2SemiboldItalic:{
+        fontFamily: 'SF Pro',
+        fontSize: 11,
+        fontWeight: 'semibold',
+        lineHeight: 13,
+        letterSpacing: -0.4,
+        color:'#000000',
+        fontStyle:'italic'
+    },
+}
+)
