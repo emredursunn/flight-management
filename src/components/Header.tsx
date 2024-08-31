629fe88cdd0adab6e8a37245af232592f6594d49
emredursunn <emredursun908@gmail.com>
2024-08-31 23:17:15 +0300

    Header added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 const Header = ({addFlight}:Props) => {
   return (
-    <View style={{width:393, height:'auto',flexDirection:'row', justifyContent:'space-between', top:59, paddingTop:3, paddingHorizontal:16, paddingBottom:8}}>
+    <View style={{width:'100%', height:'auto',flexDirection:'row', justifyContent:'space-between', top:59, paddingTop:3, paddingHorizontal:16, paddingBottom:8}}>
       <Text style={TYPOGRAPHY.largeTitleBold}>Flights</Text>
       <TouchableOpacity onPress={addFlight} style={{width:24,height:24, alignSelf:'center'}}>
         <Image source={require('../../assets/horizontal_line.png')} style={{position:'absolute',width:14,top:12,left:5,borderWidth:2,borderColor:COLORS.gray[900]}}/>
