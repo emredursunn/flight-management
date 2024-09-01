ecf3a7718b885e49a8547a743a794088bbc41700
emredursunn <emredursun908@gmail.com>
2024-09-01 03:33:22 +0300

    Header updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,11 +9,11 @@ type Props = {
 
 const Header = ({addFlight}:Props) => {
   return (
-    <View style={{width:'100%', height:'auto',flexDirection:'row', justifyContent:'space-between', top:59, paddingTop:3, paddingHorizontal:16, paddingBottom:8}}>
+    <View style={{width:'100%', height:'auto',flexDirection:'row', justifyContent:'space-between', paddingTop:3, paddingHorizontal:16, paddingBottom:8}}>
       <Text style={TYPOGRAPHY.largeTitleBold}>Flights</Text>
       <TouchableOpacity onPress={addFlight} style={{width:24,height:24, alignSelf:'center'}}>
         <Image source={require('../../assets/horizontal_line.png')} style={{position:'absolute',width:14,top:12,left:5,borderWidth:2,borderColor:COLORS.gray[900]}}/>
-        <Image source={require('../../assets/vertical_line.png')} style={{position:'absolute',height:14,top:5,left:12,borderWidth:2, borderColor:COLORS.gray[900]}}/>
+        <Image source={require('../../assets/vertical_line.png')} style={{position:'absolute',height:14,top:7,left:10,borderWidth:2, borderColor:COLORS.gray[900]}}/>
       </TouchableOpacity>
     </View>
   )
