fc2cbe2efd0c06566d1a8e11a22ccea701eb3dbf
emredursunn <emredursun908@gmail.com>
2024-09-01 18:44:39 +0300

    style changes for  bottomtab nav

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -1,14 +1,85 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import FlightsPage from "../screens/FlightsPage";
+import ProfilePage from "../screens/ProfilePage";
+import PlansPage from "../screens/PlansPage";
+import { Image } from "react-native";
+import COLORS from "../constants/colors";
 
-const Tab = createBottomTabNavigator();
+export type TabParams = {
+  Flights: undefined;
+  Plans: undefined;
+  Me: undefined;
+};
+
+const Tab = createBottomTabNavigator<TabParams>();
 
 export const Tabs = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Flights" component={FlightsPage} />
-      {/* <Tab.Screen name="Settings" component={SettingsScreen} /> */}
-      {/* <Tab.Screen name="Settings" component={SettingsScreen} /> */}
+    <Tab.Navigator
+      initialRouteName="Flights"
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          borderTopWidth: 0.5,
+          borderTopColor: COLORS.gray[200],
+          marginBottom:34,
+          shadowColor:COLORS.white,
+        },
+        tabBarActiveTintColor: COLORS.gray[900],
+        tabBarInactiveTintColor: COLORS.gray[500],
+        tabBarLabelStyle: {
+          fontSize: 10,
+          lineHeight: 12,
+          textAlign: 'center',
+          alignSelf: 'center',
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Plans"
+        component={PlansPage}
+        options={{
+          tabBarIcon: () => (
+            <Image
+              source={require("../../assets/Calendar-Heart.png")}
+              style={{
+                width: 18.95,
+                height: 18.95,
+              }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Flights"
+        component={FlightsPage}
+        options={{
+          tabBarIcon: () => (
+            <Image
+              source={require("../../assets/BottomTabPlane.png")}
+              style={{
+                width: 18.95,
+                height: 18.95,
+              }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Me"
+        component={ProfilePage}
+        options={{
+          tabBarIcon: () => (
+            <Image
+              source={require("../../assets/User.png")}
+              style={{
+                width: 18.95,
+                height: 18.95,
+              }}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
