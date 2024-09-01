56933279e7c8e0a441c4a453964e7814116adada
emredursunn <emredursun908@gmail.com>
2024-09-01 18:46:59 +0300

    other screens created

---
+++ src/screens/PlansPage.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PlansPage = () => {
+  return (
+    <View>
+      <Text>PlansPage</Text>
+    </View>
+  )
+}
+
+export default PlansPage
+
+const styles = StyleSheet.create({})
